2eac82ff2d0e274a4f258e800b4d20bd2da27eb0
Vector Nguyen <vectornguyen76@gmail.com>
2023-07-03 11:00:30 +0700

    UPDATE enviroments

---
+++ .env.pro
@@ -0,0 +1,16 @@
+# APP configuration
+APP_NAME=Flask API Rest Template
+APP_ENV=production
+
+# Flask Configuration
+API_ENTRYPOINT=app:app
+APP_SETTINGS_MODULE=config.ProductionConfig
+
+# API service configuration
+API_HOST=0.0.0.0
+
+# Database service configuration
+DATABASE_URL=sqlite:///production.db
+
+# Deployment platform
+PLATFORM_DEPLOY=EC2-AWS
--- config.py
+++ config.py
@@ -8,30 +8,31 @@ class DefaultConfig:
     """
     Default Configuration
     """
-    # Configuracion Flask
+    # Flask Configuration
     APP_NAME = os.environ.get('APP_NAME')
     SECRET_KEY = secrets.token_urlsafe(64)
     PROPAGATE_EXCEPTIONS = True
     DEBUG = False
     TESTING = False
 
-    # Configuracion de Flask-JWT-Extend
+    # Configuration of Flask-JWT-Extended
     JWT_SECRET_KEY = secrets.token_urlsafe(64)
-    # Determina los minutos que permanece activo el access token
+    # Determines the minutes that the access token remains active
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=30)
-    # Determina los dias que permanece activo el refresh token
+    # Determines the days that the refresh token remains active
     JWT_REFRESH_TOKEN_EXPIRES = datetime.timedelta(days=30)
-    # Algoritmo usado para generar el token
+    # Algorithm used to generate the token
     JWT_ALGORITHM = 'HS256'
-    # Algoritmo usado para decodificar el token
+    # Algorithm used to decode the token
     JWT_DECODE_ALGORITHMS = 'HS256'
-    # Header que debe contener el JWT en una request
+    # Header that should contain the JWT in a request
     JWT_HEADER_NAME = "Authorization"
-    # Palabra que va delante del token en la cabecera Authorization en este caso vacia
+    # Word that goes before the token in the Authorization header in this case empty
     JWT_HEADER_TYPE = 'Bearer'
-    # Dónde buscar un JWT al procesar una solicitud.
+    # Where to look for a JWT when processing a request.
     JWT_TOKEN_LOCATION = 'headers'
     
+    # Config API documents
     API_TITLE = "Template REST API"
     API_VERSION = "v1"
     OPENAPI_VERSION = "3.0.3"
@@ -39,11 +40,11 @@ class DefaultConfig:
     OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
     OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-    # Configuracion de BBDD
+    # Database configuration
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SHOW_SQLALCHEMY_LOG_MESSAGES = False
 
-    # Entornos App
+    # App Environments
     APP_ENV_LOCAL = 'local'
     APP_ENV_TESTING = 'testing'
     APP_ENV_DEVELOP = 'develop'
@@ -57,57 +58,53 @@ class DefaultConfig:
 
 
 class DevelopConfig(DefaultConfig):
-    # Entorno App
+    # App environment
     APP_ENV = DefaultConfig.APP_ENV_DEVELOP
 
-    # Activa el modo debug
+    # Activate debug mode
     DEBUG = True
 
-    # Configuracion de BBDD
+    # Database configuration
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(DefaultConfig):
-    # Entorno App
+    # App environment
     APP_ENV = DefaultConfig.APP_ENV_TESTING
 
-    # Flask deshabilita la captura de errores durante el manejo de peticiones para obtener mejores
-    # informes de error en los tests
+    # Flask disables error catching during request handling for better error reporting in tests
     TESTING = True
-    # Activa el modo debug
-    DEBUG = True  # Antes era False
-    # False para deshabilitar la protección CSRF durante los tests
+    
+    # Activate debug mode
+    DEBUG = True 
+    
+    # False to disable CSRF protection during tests
     WTF_CSRF_ENABLED = False
 
     # Logging
     LOG_FILE_API = f'{basedir}/logs/api_tests.log'
 
-    # Configuracion de BBDD
+    # Database configuration
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class LocalConfig(DefaultConfig):
-    # Entorno App
+    # App environment
     APP_ENV = DefaultConfig.APP_ENV_LOCAL
 
-    # Activa el modo debug
+    # Activate debug mode
     DEBUG = False
 
-    # Configuracion de BBDD
+    # Database configuration
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class ProductionConfig(DefaultConfig):
-    # Entorno App
+    # App environment
     APP_ENV = DefaultConfig.APP_ENV_PRODUCTION
 
-    # Activa el modo debug
+    # Activate debug mode
     DEBUG = False
 
-    # Configuracion de BBDD
+    # Database configuration
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-
-    # SQLAlchemy 1.4.x has removed support for the postgres:// URI scheme, which is used by Heroku Postgres.
-    # To maintain compatibility, perform the following before setting up connections with SQLAlchemy:
-    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
